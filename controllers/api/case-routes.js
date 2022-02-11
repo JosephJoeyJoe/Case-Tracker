c5b40c3993f599103ca3301cec7470ef3195ba22
jclong <j.mail1014@gmail.com>
2022-02-10 19:34:41 -0500

    added case-routes api

---
+++ controllers/api/case-routes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { Case } = require("../../models");
+// const withAuth = require('../../utils/auth');
+
+router.get("/", (req, res) => {
+  Case.findAll()
+    .then((dbCaseData) => res.json(dbCaseData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Case.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCaseData) => {
+      if (!dbCaseData) {
+        res.status(404).json({ message: "No case found with this id!" });
+        return;
+      }
+      res.json(dbCaseData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
