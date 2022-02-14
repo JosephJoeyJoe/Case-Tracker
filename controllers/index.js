783abf087bb76eee8cf73cb51ad3f4276d1b695d
jclong <j.mail1014@gmail.com>
2022-02-14 18:30:19 -0500

    added dashboard and index routes to api/index

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-// const for apiRoutes = require('./api');
+const apiRoutes = require('./api');
 const homepage = require('./homepage-routes');
-// const for dashboard = require('./user-dashboard-routes');
+const dashboard = require('./user-dashboard-routes');
 const router = require("express").Router();
 
 router.use('/', homepage);
-// router.use('/api', instest api const); 
-// router.use('/dashboard', dashboard);
+router.use('/api', apiRoutes); 
+router.use('/dashboard', dashboard);
 
 router.use((req, res) => {
     res.status(404).end();
