221555f158cc51561bbe893cb036a9f520c447aa
jclong <j.mail1014@gmail.com>
2022-02-19 13:18:23 -0500

    change login

--- controllers/api/manager-routes.js
+++ controllers/api/manager-routes.js
@@ -33,7 +33,7 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/login", (req, res) => {
   Manager.findOne({
     where: {
       email: req.body.email,
