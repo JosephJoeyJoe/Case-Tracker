34c3c666a0afd8646c014d73f6d3beeb7b90e619
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-02-20 17:21:32 -0500

    merge conflicts

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,80 +1,64 @@
 const router = require("express").Router();
-// const req = require("express/lib/request");
-// const res = require("express/lib/response");
+const { Router } = require("express");
 const sequelize = require("../config/connection");
-const { Employee, Case, Manager } = require("../models");
+const { Employee, Manager } = require("../models");
 
-// router.get('/', (req, res) => {
-//   Case.findAll({
-//       attributes: ['id'],
-//       include: [{
-//           model: Employee,
-//           attributes: ['id', 'case_id', 'manager_id', 'last_day', 'symptom_start'],
-//           include: {
-//               model: Manager,
-//               attributes: ['id']
-//           }
-//       }]
-//   })
-//   .then(caseData => {
-//       const cases = caseData.map(cases => cases.get({
-//           plain: true
-//       }));
-//   res.render('login', {
-//       cases,
-//       loggedIn: req.session.loggedIn
-//       });
-//   })
-//   .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//   })
-// })
-
-// router.get('/case/:id', (req, res) => {
-//   Case.findOne({
-//     where: {
-//         id: req.params.id
-//       },
-//       attributes: ['id'],
-//       include: [{
-//           model: Employee,
-//           attributes: ['id', 'case_id', 'manager_id', 'last_day', 'symptom_start'],
-//           include: {
-//               model: Manager,
-//               attributes: ['id']
-//           }
-//       }]
-//   })
-//   .then(caseData => {
-//       if (!caseData) {
-//           res.status(404).json({ message: 'No case found with that id'});
-//           return;
-//       }
-//       const oneCase = caseData.get({plain: true});
-//       res.render('one-case', {oneCase, loggedIn: req.session.loggedIn});
-//   })
-//   .catch(err => {
-//       console.loggedIn(err);
-//       res.status(500).json(err);
-//   })
-// })
-
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/dashboard');
-      return;
-    }
-  
-    res.render('login');
+router.get("/", (req, res) => {
+  Employee.findAll({
+    attributes: ["id", "manager_id", "last_day", "symptom_start"],
+    include: {
+      model: Manager,
+      attributes: ["id"],
+    },
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
   });
-  
-  router.get('/signup', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/login');
-      return;
-    }
-  
-    res.render('signup');
-  });
+});
+
+router.get("/employee/:id", (req, res) => {
+  Employee.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "manager_id", "last_day", "symptom_start"],
+        include: {
+          model: Manager,
+          attributes: ["id"],
+        },
+      })
+    .then((employeeData) => {
+      if (!employeeData) {
+        res.status(404).json({ message: "No employee found with that id" });
+        return;
+      }
+      const oneEmployee = employeeData.get({ plain: true });
+      res.render("one-employee", { oneEmployee, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.loggedIn(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+//changes route to /dashboard instead of /
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("signup");
+});
+
 module.exports = router;
